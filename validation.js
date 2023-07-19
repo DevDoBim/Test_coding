58c5cc7f85366624a01b625cdba0d6339251c06d
DevDoBim <zmdkdna2@naver.com>
2023-07-19 16:04:18 +0900

    feat : 정규 표현식을 이용한 테스트 추가

--- validation.js
+++ validation.js
@@ -2,24 +2,26 @@ module.exports = {
   // # 입력한 이메일 주소에는 "@" 문자가 1개만 존재해야 이메일 형식이다.
   // # 입력한 이메일 주소에 공백(스페이스)이 존재하면 이메일 형식이 아니다.
   // # 입력한 이메일 주소 맨 앞에 하이픈(-)이 있으면 이메일 형식이 아니다.
+  // # 입력한 이메일 주소중, 로컬 파트(골뱅이 기준 앞부분)에는 영문 대소문자와 숫자, 특수문자는 덧셈기호(+),
+  // 하이픈(-), 언더바(_) 3개 외에 다른 값이 존재하면 이메일 형식이 아니다.
+  // # 입력한 이메일 주소중, 도메인(골뱅이 기준 뒷부분)에는 영문 대소문자와 숫자, 하이픈(-) 외에 다른 값이 존재하면 이메일 형식이 아니다.
+
   isEmail: (value) => {
-    // || : 왼쪽이 값이 있을 때는 value 반환, 없을 때는 오른쪽 값 반환
-    // # value 값이 없을 때 오류 대비
     const email = value || '';
+    const [localPart, domain, ...etc] = email.split('@');
 
-    // # .split('') ''을 기준으로 문자열을 나눈다.
-    // "test@email.com" => @ => ["test", "gmail.com"]
-    if (email.split('@').length !== 2) {
-        // email을 split한 값이 2가 아니면 @없거나 @2개 이상
-        return false;
-    } else if(email.includes(' ')){
-        // email 안에 공백이 존재
-        return false;
-    } else if(email[0] === '-'){
-        // email의 첫 문자열이 - 일때
-        return false;
+    if (!localPart || !domain || etc.length) {
+      return false;
+    } else if (email.includes(' ')) {
+      return false;
+    } else if (email[0] === '-') {
+      return false;
+    } else if (!/^[a-z0-9+_-]+$/gi.test(localPart)) {
+      return false;
+    } else if (!/^[a-z0-9.-]+$/gi.test(domain)) {
+      return false;
     }
-    return true;
 
+    return true;
   },
 };
